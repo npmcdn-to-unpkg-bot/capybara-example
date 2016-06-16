ea98274e4e66ecef72b5271e829b0e0d4076d5a1
Jennifer Stern <jstern@instructure.com>
2016-06-15 21:50:08 -0500

    Add comments and TODOs
    Change-Id: I96d67ca136b6066513a9746ee8f022f64764bc01

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,9 +4,22 @@ feature "Log in to Dashboard" do
   scenario "filling out the log in form correctly displays the dashboard" do
     visit '/'
 
+    # locate elements via:
+    # tag, role, value, aria-describedby, aria-labelledby
+
+    # scope via:
+    # role, fieldset + legend, form
+
     # use aria role attributes and tags to verify text content
     expect(find("[role='banner'] h1")).to have_content 'Example'
     expect(find("[role='contentinfo']")).to have_content '© 2016'
+
+    within "[role='navigation']" do
+      expect(page).to have_selector('a', count: 2)
+
+      expect(find("a:nth-of-type(1)")).to have_content 'Instructure'
+      expect(find("a:nth-of-type(2)")).to have_content 'Github'
+    end
 
     # scope form fields to a specific form using tag + aria-label
     within "form[aria-label='Log In']" do
@@ -23,6 +36,10 @@ feature "Log in to Dashboard" do
     within "[role='main']" do
       expect(page).to have_content 'Welcome user@example.com'
     end
+
+    # TODO: show locating an input via label + value
+    # TODO: show locating an input within a fieldset (+ aria-describedby and legend)
+
   end
 end
 
