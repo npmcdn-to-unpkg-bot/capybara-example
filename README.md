a99c4d67591d73d7b5ff74d623bc64d238fe876f
Jennifer Stern <jstern@instructure.com>
2016-07-07 13:27:38 -0500

    Added some more examples and links for more info
    Change-Id: I6d70d6ac59ff57c20c36d834adcfb74dc2542a07

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ bundle install
 ```
 bundle exec rspec spec
 ```
+
+
+## for more information see:
+
+- html 5 semantic markup: https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+- html 5 forms: http://www.html5rocks.com/en/tutorials/forms/html5forms/
+- aria: https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA
+- sizzle selectors: https://github.com/jquery/sizzle/wiki
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -10,23 +10,32 @@ feature "Log in to Dashboard" do
     # scope via:
     # role, fieldset + legend, form
 
-    # use aria role attributes and tags to verify text content
+    # use aria role attributes and *semantic* tags to verify text content
     expect(find("[role='banner'] h1")).to have_content 'Example'
     expect(find("[role='contentinfo']")).to have_content '© 2016'
 
     within "[role='navigation']" do
       expect(page).to have_selector('a', count: 2)
 
-      expect(find("a:nth-of-type(1)")).to have_content 'Instructure'
-      expect(find("a:nth-of-type(2)")).to have_content 'Github'
+      expect(find("li:nth-of-type(1) a")).to have_content 'Instructure'
+      expect(find("li:nth-of-type(2) a")).to have_content 'Github'
+
+      # requires jquery sizzle selectors:
+      # expect(find("a:contains('Instructure')")).to exist
+      # expect(find("a:contains('Github')")).to exist
     end
 
-    # scope form fields to a specific form using tag + aria-label
+    # scope form fields to a specific form using *semantic* tag + aria-label
     within "form[aria-label='Log In']" do
 
       # use label text to locate form fields
       fill_in 'Email', :with => 'user@example.com'
       fill_in 'Password', :with => 'password'
+
+      # select inputs by type
+      expect(find("input[type='email']").value).to eq 'user@example.com'
+
+      # and another way:
 
       # use tag + text label to locate buttons
       click_button 'Log In'
@@ -35,11 +44,11 @@ feature "Log in to Dashboard" do
     # locate content within a particular location using aria role attributes
     within "[role='main']" do
       expect(page).to have_content 'Welcome user@example.com'
+
+      # the structure of table markup should be pretty stable so we can write selectors like:
+      # requires jquery sizzle selectors:
+      # expect(find("caption:contains('User data') ~ tbody > tr:nth-child(1) > td:nth-child(2)")).to have_content 'Somebody'
     end
-
-    # TODO: show locating an input via label + value
-    # TODO: show locating an input within a fieldset (+ aria-describedby and legend)
-
   end
 end
 
